b1be0b830fef930e64503190a5e083815304b04f
Kev-003 <kevern920@gmail.com>
2026-02-04 15:10:39 +0800

    feat: Implement document request and signing APIs

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', \App\Http\Middleware\EnsureUserBelongsToBarangay::class])->group(function () {
+
+    // The Request Endpoint
+    Route::post('/barangay/{barangay_id}/documents/request', [DocumentController::class, 'request']);
+
+    // The Sign Endpoint
+    Route::patch('/barangay/{barangay_id}/documents/{transaction_id}/sign', [DocumentController::class, 'sign']);
+
 });
